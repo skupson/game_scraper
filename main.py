83dafbba6234ad5b2912672660e3342e0469c04d
skupson <andrija@tacticalsoft.rs>
2020-07-19 23:47:29 +0200

    Created the package Scrapers and moved file into it

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 
-from ravnica_scrape import ravnica_scrape
-from conflux_scrape import conflux_scrape
+from Scrapers.ravnica_scrape import ravnica_scrape
 
 if __name__ == "__main__":
    ravnica_scrape()

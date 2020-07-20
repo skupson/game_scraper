8442e063244e42121bdeca1a8796a2091109d74f
skupson <andrija.skupinski.1.16@ict.edu.rs>
2020-07-20 17:39:49 -0400

    Changed the data type to list

--- Scrapers/ravnica_scrape.py
+++ Scrapers/ravnica_scrape.py
@@ -4,13 +4,13 @@ from bs4 import BeautifulSoup
 def ravnica_scrape():
     html_page = requests.get("https://ravnicabg.rs/shop/")
     page_number = 1
-    ravnica_data = {}
+    ravnica_data = []
     while html_page.status_code == 200:
         soup = BeautifulSoup(html_page.content, 'html.parser')
         game_names = soup.select(".woocommerce-loop-product__title")
         game_prices = soup.select(".woocommerce-Price-amount")
         for index, name in enumerate(game_names):
-            ravnica_data[index] = {"name": name.text, "price": game_prices[index].text}
+            ravnica_data.append({"name": name.text, "price": game_prices[index].text})
         page_number += 1
         html_page = requests.get(f"https://ravnicabg.rs/shop/page/{page_number}/")
     print(ravnica_data)
